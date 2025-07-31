ed40c1bb3e487a4aa3d740bc2e65ca581387c958
Ehthesham-Ahmed <117994804+Ehthesham-Ahmed@users.noreply.github.com>
2025-07-31 12:30:08 +0530

    theme context

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+import ThemeContext from '@/contexts/ThemeContext';
+import { useEffect, useState } from 'react';
+
+export default function ThemeToggle() {
+    return (
+        <div>ThemeToggle</div>
+    )
+}
